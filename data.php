a680e59ee33180f82bee8f75712f67a8427c8dff
Alifiaardita <110959884+Alifiaardita@users.noreply.github.com>
2022-09-16 11:19:12 +0700

    Add files via upload

---
+++ data.php
@@ -0,0 +1,51 @@
+<?php 
+
+    $user = [
+        [
+            "username" => "admin1",
+            "password" => "admin1",
+            "nama" => "Alifia Ardita",
+            "img" => "foto1",
+            "alamat" => "mejoyo gg 2",
+            "tlp" => "08453521144", 
+            "role" => "admin"
+        ],
+        [
+            "username" => "admin2",
+            "password" => "admin2",
+            "nama" => "Alifia Ardita",
+            "img" => "foto2",
+            "alamat" => "mejoyo gg 3",
+            "tlp" => "08574634897",
+            "role" => "admin"
+        ],
+        [
+            "username" => "siswa1",
+            "password" => "siswa1",
+            "nama" => "Alifia Ardita",
+            "img" => "foto3",
+            "alamat" => "jatisari gg muholla",
+            "tlp" => "08725765878",
+            "role" => "siswa"
+        ],
+        [
+            "username" => "siswa2",
+            "password" => "siswa2",
+            "nama" => "Alifia Ardita",
+            "img" => "foto4",
+            "alamat" => "jatisari gg mawar",
+            "tlp" => "09464575676",
+            "role" => "siswa"
+        ],
+        [
+            "username" => "siswa3",
+            "password" => "siswa3",
+            "nama" => "Alifia Ardita",
+            "img" => "foto5",
+            "alamat" => "jatisari gg anggrek",
+            "tlp" => "094574357688",
+            "role" => "siswa"
+        ],
+    ];
+
+?>
